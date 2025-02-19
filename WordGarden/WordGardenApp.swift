c6b791c76b63cfce21544ff15798ccae363626b7
Chris Wheeler <cwheeler@emeals.com>
2025-02-19 17:44:43 -0500

    Initial Commit

---
+++ WordGarden/WordGardenApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordGardenApp.swift
+//  WordGarden
+//
+//  Created by Chris Wheeler on 2/19/25.
+//
+
+import SwiftUI
+
+@main
+struct WordGardenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
